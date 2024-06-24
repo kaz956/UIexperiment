35554e7430fe0a86c7310e6d5c81653af5fefbd1
kaz956 <kazukichi956@keio.jp>
2024-06-25 01:07:53 +0900

    ハンバーガーメニュー

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
+/*---------- ハンバーガーメニュー ----------*/
+const hamburger = document.querySelector(".js_hamburger");
+const navigation = document.querySelector(".js_navigation");
+const body = document.querySelector(".js_body");
 
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-active");
+    navigation.classList.toggle("is-active");
+    body.classList.toggle("is-active");
+});
+
+const navLinks = document.querySelectorAll(".l_header-nav_list");
+navLinks.forEach(navLink => {
+    navLink.addEventListener("click", () => {
+        hamburger.classList.remove("is-active");
+        navigation.classList.remove("is-active");
+        body.classList.remove("is-active");
+    });
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     const taskTitle = document.getElementById('task-title');
